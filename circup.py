bff126c93473060cb5796470a20eb4d5e06cdfe8
askpatricw <4002194+askpatrickw@users.noreply.github.com>
2021-01-23 18:58:50 -0800

    clean up comments, ran make check

--- circup.py
+++ circup.py
@@ -22,11 +22,6 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
 
-import inspect
-
-def lineno():
-    """Returns the current line number in our program."""
-    return inspect.currentframe().f_back.f_lineno
 
 import ctypes
 import glob
@@ -538,38 +533,45 @@ def get_dependencies2(*requested_libraries, mod_names, to_install=()):
     """
     if not requested_libraries[0]:
         return to_install
-    else:
-        _to_install = to_install
-        _requested_libraries = []
-        _rl = requested_libraries[0]
-        for l in _rl:
-            _requested_libraries.append(l)
-        for library in _requested_libraries:
-            library = library.lower()
-            if library not in _to_install:
-                _to_install = _to_install + (library,)
-                # get the library repo name from the full .git repo URL
-                if "__repo__" in mod_names[library].keys():
-                    library_repo_name = mod_names[library]["__repo__"].split("/")[-1].split(".")[0]
-                    requirements_base_url = "https://raw.githubusercontent.com/adafruit/"
-                    requirements_file_path = "master/requirements.txt"
-                    requirements_url = (
-                        f"{requirements_base_url}{library_repo_name}/{requirements_file_path}"
+    _to_install = to_install
+    _requested_libraries = []
+    _rl = requested_libraries[0]
+    for l in _rl:
+        _requested_libraries.append(l)
+    for library in _requested_libraries:
+        library = library.lower()
+        if library not in _to_install:
+            _to_install = _to_install + (library,)
+            # get the library repo name from the full .git repo URL
+            if "__repo__" in mod_names[library].keys():
+                library_repo_name = (
+                    mod_names[library]["__repo__"].split("/")[-1].split(".")[0]
+                )
+                requirements_base_url = "https://raw.githubusercontent.com/adafruit/"
+                requirements_file_path = "master/requirements.txt"
+                requirements_url = (
+                    requirements_base_url
+                    + library_repo_name
+                    + "/"
+                    + requirements_file_path
+                )
+                response = requests.get(requirements_url)
+                if response.status_code == 200:
+                    _requested_libraries.extend(
+                        libraries_from_requirements(response.text)
                     )
-                    response = requests.get(requirements_url)
-                    if response.status_code == 200:
-                        _requested_libraries.extend(libraries_from_requirements(response.text))
-                else:
-                    click.echo(
-                        f"{library} library has no __repo__ metadata. "
-                        "Circup cannot install its dependencies.", err=True
-                        )
-            _requested_libraries.remove(library)
-            return get_dependencies2(
-                tuple(_requested_libraries),
-                mod_names=mod_names,
-                to_install=_to_install,
-            )
+            else:
+                click.echo(
+                    f"{library} library has no __repo__ metadata. "
+                    "Circup cannot install its dependencies.",
+                    err=True,
+                )
+        _requested_libraries.remove(library)
+        return get_dependencies2(
+            tuple(_requested_libraries),
+            mod_names=mod_names,
+            to_install=_to_install,
+        )
 
 
 def get_dependencies(module_repo):
@@ -916,18 +918,6 @@ def install(ctx, modules, py, requirement):  # pragma: no cover
         requested_installs = libraries_from_requirements(requirements_txt)
     else:
         requested_installs = modules
-        # to_install = []
-        # for request in requested_installs:
-        #     request = request.lower() if request else ""
-        #     to_install.append(request)
-        #     dependencies = get_dependencies(mod_names[request]["__repo__"])
-        #     for dependency in dependencies:
-        #         if dependency not in to_install:
-        #             to_install.append(dependency)
-        #             more_dependencies = get_dependencies(mod_names[dependency]["__repo__"])
-        #             for even_more in more_dependencies:
-        #                 if even_more not in to_install:
-        #                     to_install.append(even_more)
     to_install = get_dependencies2(requested_installs, mod_names=mod_names)
     for library in to_install:
         install_module(ctx.obj["DEVICE_PATH"], library, py, mod_names)

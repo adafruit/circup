a634f227b0c0e9232793742f11f08a73b27bdcce
Patrick <4002194+askpatrickw@users.noreply.github.com>
2021-04-08 08:35:25 -0700

    Add support for CP7 MPY bundles

--- circup.py
+++ circup.py
@@ -463,6 +463,11 @@ def get_bundle(tag):
             "releases/download"
             "/{tag}/adafruit-circuitpython-bundle-6.x-mpy-{tag}.zip".format(tag=tag)
         ),
+        "7mpy": (
+            "https://github.com/adafruit/Adafruit_CircuitPython_Bundle/"
+            "releases/download"
+            "/{tag}/adafruit-circuitpython-bundle-7.x-mpy-{tag}.zip".format(tag=tag)
+        ),
     }
     click.echo("Downloading latest version information.\n")
     for platform, url in urls.items():

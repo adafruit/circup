1bc332b8394b3afde7c3b10ee970fb5016cf750c
Neradoc <neraOnGit@ri1.fr>
2024-12-29 19:56:10 +0100

    fix sphinx warning

--- docs/conf.py
+++ docs/conf.py
@@ -109,7 +109,6 @@ if not on_rtd:  # only import and set the theme if we're building docs locally
         import sphinx_rtd_theme
 
         html_theme = "sphinx_rtd_theme"
-        html_theme_path = [sphinx_rtd_theme.get_html_theme_path(), "."]
     except:
         html_theme = "default"
         html_theme_path = ["."]
